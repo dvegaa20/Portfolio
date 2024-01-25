73b875491dfa78ac3e23d43c35dd651514918636
dvegaa20 <diego.vega.camacho@gmail.com>
2024-01-25 14:09:40 -0600

    all over again

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import { Home, About, Skills, Portfolio, Testimonials } from './containers/index'
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="skills" element={<Skills />} />
-      <Route path="portfolio" element={<Portfolio />} />
-      <Route path="testimonials" element={<Testimonials />} />
-    </Routes>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/containers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './Home'
-export { default as About } from './About'
-export { default as Skills } from './Skills'
-export { default as Portfolio } from './Portfolio'
-export { default as Testimonials } from './Testimonials'
