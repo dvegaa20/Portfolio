13ff711837ac3ed4c9b1109229b434a80258e34e
dvegaa20 <diego.vega.camacho@gmail.com>
2024-01-30 23:57:37 -0600

    start over

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,11 @@ import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import Logo from '../../assets/logo.svg'
 import './navbar.css'
 
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { GrInstagram } from "react-icons/gr";
+
 const Menu = () => (
   <>
   <p><a href="about">About</a></p>
@@ -16,33 +21,18 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <div className="me__navbar">
-      <div className="me__navbar-links-logo">
-        <img src={Logo} alt="logo" />
-      </div>
-      <div className="me__navbar-links">
-        <div className="me__navbar-links-container">
+    <div className='pw__header'>
+      <div className='pw__navbar'>
+        <div className='pw__logo-links'>
+          <img src={Logo} alt="" />
           <Menu />
-        </div>
-      </div>
-      <div className="me__navbar-download">
-        <button type='button'>Download CV</button>
-      </div>
-      <div className="me__navbar-menu">
-        {toggleMenu
-          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
-          : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
-        }
-        {toggleMenu && (
-          <div className="me__navbar-menu-container scale-up-center">
-            <div className="me__navbar-menu__container-links">
-              <Menu />
-              <div className="me__navbar-menu__container-links-download">
-                <button type='button'>Start for free</button>
-              </div>
-            </div>
+          <div className='pw__social'>
+            <a href="https://www.linkedin.com/in/rafael-oliveira-4b6b8a1b5/"><FaLinkedin /></a>
+            <a href="https://www.linkedin.com/in/rafael-oliveira-4b6b8a1b5/"><FaGithub /></a>
+            <a href="https://www.linkedin.com/in/rafael-oliveira-4b6b8a1b5/"><SiGmail /></a>
+            <a href="https://www.linkedin.com/in/rafael-oliveira-4b6b8a1b5/"><GrInstagram /></a>
           </div>
-        )}
+        </div>
       </div>
     </div>
   )
--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar  from '../../components/navbar/Navbar'
 import './home.css'
 
+import { MdArrowOutward } from "react-icons/md";
+
 const Home = () => {
   return (
     <div>
