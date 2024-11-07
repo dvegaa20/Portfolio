09bd59e68d48a1b9a9542f9dc7126592c33d03ec
dvegaa20 <diego.vega.camacho@gmail.com>
2024-11-07 09:03:59 -0600

    Removed all files to start fresh

--- src/containers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './home/Home'
-export { default as About } from './about/About'
-export { default as Skills } from './skills/Skills'
-export { default as Portfolio } from './portfolio/Portfolio'
-export { default as Testimonials } from './testimonials/Testimonials'
