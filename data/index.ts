ede05b602454cbe35d9cbfd36fe4c9498ee8777b
dvegaa20 <diego.vega.camacho@gmail.com>
2024-12-06 14:53:34 -0600

    style(portfolio): added new elements

--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,5 @@
+import { link } from "fs";
+
 // Navigation items //
 export const navItems = [
   { name: "About", link: "#about" },
@@ -52,12 +54,12 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
-    description: "The Inside Scoop",
+    title: "Currently building a dental patient management system.",
+    description: "Here’s what i’m up to lately",
     className: "md:col-span-3 md:row-span-2",
-    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    imgClassName: "absolute -right-10 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
+    img: "/dentist.svg",
     spareImg: "/grid.svg",
   },
   {
@@ -76,11 +78,12 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "https://github.com/adrianhajdin?tab=repositories",
+    title: "TDR Dashboard - Fleet Management",
+    des: "Developed an interactive dashboard for fleet management to optimize preventive maintenance and reduce costs by 15%.",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/py.svg", "/dash.svg", "/js.svg", "/tail.svg"],
+    linkRepo: "https://github.com/dvegaa20/TDR-Dashboard",
+    linkProyect: "https://tdr-dashboard.vercel.app/",
   },
   {
     id: 2,
@@ -88,7 +91,8 @@ export const projects = [
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
     img: "/p2.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/zoom-clone",
+    linkRepo: "https://github.com/adrianhajdin/zoom-clone",
+    linkProyect: "",
   },
   {
     id: 3,
@@ -96,7 +100,8 @@ export const projects = [
     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
     img: "/p3.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/ai_saas_app",
+    linkRepo: "https://github.com/adrianhajdin/ai_saas_app",
+    linkProyect: "",
   },
   {
     id: 4,
@@ -104,7 +109,8 @@ export const projects = [
     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
     img: "/p4.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "https://github.com/adrianhajdin/iphone",
+    linkRepo: "https://github.com/adrianhajdin/iphone",
+    linkProyect: "",
   },
 ];
 
@@ -180,28 +186,28 @@ export const workExperience = [
     title: "Frontend Engineer Intern",
     desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
     className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
+    thumbnail: "/pc.svg",
   },
   {
     id: 2,
     title: "Mobile App Dev - JSM Tech",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
+    thumbnail: "/rocket.svg",
   },
   {
     id: 3,
     title: "Freelance App Dev Project",
     desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
+    thumbnail: "/bulb.svg",
   },
   {
     id: 4,
     title: "Lead Frontend Developer",
     desc: "Developed and maintained user-facing features using modern frontend technologies.",
     className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
+    thumbnail: "/internet.svg",
   },
 ];
 
